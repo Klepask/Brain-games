0f04b32b519b5c0b7b921e3c7c9883feb89ed87e
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-05-25 18:17:59 +0300

    chaanged brain-games.js, package.json

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync'
+import { userName } from '/Users/evvva/desktop/hexlet/frontend-project-44/src/cli.js' 
+
 console.log('Welcome to the Brain Games!');
-
-
