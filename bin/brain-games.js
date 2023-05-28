e32280bf70ba56c2f3ca29cab567bd9084a6133d
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-05-29 02:15:27 +0300

    update brain-even, brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync'
 import { welcome } from '/Users/evvva/desktop/hexlet/frontend-project-44/src/cli.js'
-
-
-
-#!/usr/bin/env node
-import readlineSync from 'readline-sync'
-import welcome from '../src/cli.js'
