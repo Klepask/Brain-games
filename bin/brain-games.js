cd3ee0ff9224ab3ed4af2aad5219b66bdc6f6190
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-05-28 21:44:59 +0300

    update brain-games.js and cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync'
-import { userName } from '/Users/evvva/desktop/hexlet/frontend-project-44/src/cli.js' 
+import { userName } from '/Users/evvva/desktop/hexlet/frontend-project-44/src/cli.js'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!')
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+const randomNumber = Math.ceil(Math.random() * 100)
+const str = readlineSync.question('Question: ' + randomNumber + ' ') 
+const arr2 = str.split(' ')
+const answer = arr2[arr2.length - 1]
+
+let i = 0;
+while (i < 3) {
+    if (number % 2 === 0 && answer === 'yes' || number % 2 !== 0 && answer === 'no') {
+        console.log('Your answer: ' + number)
+        console.log('Correct!')
+        i++;
+    } else {
+        console.log('Your answer: ' + number)
+        if (answer === 'yes') {
+            console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`)
+        } else if (answer === 'no') {
+            console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.`)
+        }
+        console.log(`Let's try again, ${userName}!`)
+    }
+    console.log(`Congratulations, ${userName}!`);
+}
