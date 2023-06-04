c4e9c06f10055651fd9240d535714998b7051461
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-06-04 18:16:11 +0300

    update Makefile, .gitignore, add brain-prime.js

---
+++ bin/games/brain-prime.js
@@ -0,0 +1,29 @@
+import { randomNumber, engineGames } from '../index.js';
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  const arr = [];
+  for (let i = 1; i <= num; i += 1) {
+    if (num % i === 0) {
+      arr.push(i);
+    }
+  }
+
+  if (arr.length === 2) {
+    return true;
+  }
+
+  return false;
+};
+
+const getGameData = () => {
+  const number = randomNumber(2, 100);
+  const question = number;
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const primeGame = () => engineGames(gameRules, getGameData);
+
+export default primeGame;
