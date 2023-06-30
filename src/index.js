e9c2bddd63d5b3753337b152a0ae0cf9884c4d89
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-07-01 00:14:13 +0300

    update brain-even.js, brain-gcd.js, brain-progression.js, even.js, index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import readlineSync from 'readline-sync';
+
 const roundCounter = 2;
 
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const engineGames = (gameRules, getGameData) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(gameRules);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameRules);
 
-    for (let i = 0; i <= roundCounter; i += 1) { 
-      const [question, correctAnswer] = getGameData();
-      console.log(`Question: ${question}`);
-      const playerAnswer = readlineSync.question('You answer: ');
-      if (correctAnswer === playerAnswer) {
-        console.log('Correct!');
-      } else {
-        console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-        console.log(`Let's try again, ${userName}!`);
-        return;
-      }
+  for (let i = 0; i <= roundCounter; i += 1) { 
+    const [question, correctAnswer] = getGameData();
+    console.log(`Question: ${question}`);
+    const playerAnswer = readlineSync.question('You answer: ');
+    if (correctAnswer === playerAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+  }
+  console.log(`Congratulations, ${userName}!`);
 
-    console.log(`Congratulations, ${userName}!`);
-  };
+};
 
-  export default engineGames;
-  
-  export { randomNumber, roundCounter, engineGames };
+export default engineGames;
+
+export { randomNumber, roundCounter, engineGames };
