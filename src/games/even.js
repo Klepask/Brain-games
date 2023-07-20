1347b9c39bdb2f374ee0b2c30111fab0dc021fea
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-07-20 23:22:00 +0300

    update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,7 @@ import { getRandomNumber } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-};
+const isEven = (number) => number % 2 === 0;
 
 const getGameData = () => {
   const number = getRandomNumber(1, 100);
