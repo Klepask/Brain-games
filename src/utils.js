9eab83629635b1079de9cdfcfed7b34bde71b456
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-07-20 22:23:44 +0300

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomIndex = (length) => Math.floor(Math.random() * length);
+export const getRandomIndex = (array) => {
+  const length = array.length;
+  return Math.floor(Math.random() * length);
+}
