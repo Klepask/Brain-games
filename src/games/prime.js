b44d033db6bd3bf6d55826424b9c0c28aacbf715
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-08-12 18:24:23 +0300

    update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,8 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (number) => {
   if (number <= 1) return false;
-  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+  const limit = Math.sqrt(number);
+  for (let i = 2; i <= limit; i += 1) {
     if (number % i === 0) {
       return false;
     }
